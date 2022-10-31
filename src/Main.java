4f0cfda5e5ad9cd1b52b641c54500d9b87ef0a14
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-10-31 14:41:18 +0700

    update features

--- src/Main.java
+++ src/Main.java
@@ -12,13 +12,13 @@ public class Main {
         BorrowManagementMenu borrowManagementMenu = new BorrowManagementMenu();
         int choice = -1;
         while (choice != 0) {
-            System.out.println("======================================");
-            System.out.println("||   ==== Library Management ====   ||");
-            System.out.println("|| 1: Book Management               ||");
-            System.out.println("|| 2: Student Management            ||");
-            System.out.println("|| 3: Borrow Management             ||");
-            System.out.println("|| 0: Exit program                  ||");
-            System.out.println("======================================");
+            System.out.println("================================");
+            System.out.println("||==== Library Management ====||");
+            System.out.println("|| 1: Book Management         ||");
+            System.out.println("|| 2: Student Management      ||");
+            System.out.println("|| 3: Borrow Management       ||");
+            System.out.println("|| 0: Exit program            ||");
+            System.out.println("================================");
             try {
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("Enter choice");
@@ -32,8 +32,6 @@ public class Main {
                         break;
                     case 3:
                         borrowManagementMenu.handleMenu();
-                        break;
-                    case 0:
                         break;
                 }
             } catch (InputMismatchException e) {
